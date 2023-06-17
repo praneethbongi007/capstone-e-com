19b5392cee8d16b7a3ac35fa4ce9cfba14712ec2
praneethbongi007 <praneethbongi@gmail.com>
2023-06-17 11:18:47 +0530

    solved errors 3.0

--- src/Routes/Home/Home.component.js
+++ src/Routes/Home/Home.component.js
@@ -1,7 +1,14 @@
 import Directory from "../../components/directory/directory.component";
+import { Outlet } from 'react-router-dom';
+
 const Home = () => {
  
-  return <Directory catagories={catagories} />;
+  return (
+    <div>
+      <Directory/>
+      <Outlet/>
+    </div>
+  );
 
 };
 
--- src/components/directory-item/directory-item.component.js
+++ src/components/directory-item/directory-item.component.js
@@ -1,11 +1,11 @@
 import "./directory-item.styles.scss";
 import { useNavigate } from "react-router-dom";
 const DirectoryItem = ({ catagory }) => {
-  const { imageUrl, title,route } = catagory;
- const navigate = useNavigate()
- const onNavigationHandler=()=>navigate(route)
+  const { imageUrl, title, route } = catagory;
+  const navigate = useNavigate();
+  const onNavigationHandler = () => navigate(route);
   return (
-    <div className="directory-item-container">
+    <div className="directory-item-container" onClick={onNavigationHandler}>
       <div
         className="background-image"
         style={{
